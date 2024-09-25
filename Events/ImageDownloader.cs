48decc3825e0eae8c09257f6bd45777bc8a76c02
Loskutova Marina <mloskutova1589@gmail.com>
2024-09-25 19:42:39 +0300

    first commit

---
+++ Events/ImageDownloader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Events
+{
+    public class ImageDownloader
+    {
+        public delegate void MethodContainer(string fileName);
+
+        public event MethodContainer ImageStarted;
+        public event MethodContainer ImageCompleted;
+        public async Task DownloadAsync(string uri, string fileName, CancellationToken token)
+        {
+            HttpClient _httpClient = new HttpClient();
+            if (token.IsCancellationRequested) return;
+            if (ImageStarted != null) ImageStarted(fileName);
+
+            var response = await _httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            byte[] bytes = await response.Content.ReadAsByteArrayAsync();
+            _ = File.WriteAllBytesAsync(fileName, bytes);
+
+            if (ImageCompleted != null) ImageCompleted(fileName);
+        }
+
+        public void LoadingStarted(string fileName)
+        {
+            Console.WriteLine($"File: {fileName} download has started");
+        }
+
+        public void LoadingCompleted(string fileName)
+        {
+            Console.WriteLine($"File: {fileName} download has completed");
+        }
+
+        public void MonitorKeypress(CancellationTokenSource cts, Dictionary<string, Task> tasks)
+        {
+            ConsoleKeyInfo consoleKeyInfo;
+
+            do
+            {
+                consoleKeyInfo = Console.ReadKey(true);
+
+                if (consoleKeyInfo.Key == ConsoleKey.A) cts.Cancel();
+                else 
+                {
+                    Console.WriteLine();
+                    foreach (var tsak in tasks)
+                    {
+                        bool taskComplete = tsak.Value.IsCompleted;
+                        Console.WriteLine($"File: {tsak.Key}; loading:{taskComplete}");
+                        Task.Delay(300);
+                    }
+                }
+                if (cts.IsCancellationRequested) return;
+
+            } while (true);
+        }
+
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ImageDownloader imageDownloader = new();
+            Dictionary<string, Task> tasks = new();
+
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+            
+            Console.CursorVisible = false;
+            Task consoleKeyTask = Task.Run(() => { imageDownloader.MonitorKeypress(cancelTokenSource, tasks); });
+
+            imageDownloader.ImageStarted += imageDownloader.LoadingStarted;
+            imageDownloader.ImageCompleted += imageDownloader.LoadingCompleted;
+            try
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    int counter = i;
+                    string fileName = $"{counter}_image.jpg";
+
+                    Task sample = Task.Run(async () => await imageDownloader.DownloadAsync(
+                        "https://s1.bloknot-voronezh.ru/thumb/850x0xcut/upload/iblock/509/0d1587dc21_7605080_8213488.jpg",
+                        fileName,
+                        token));
+                    tasks.Add(fileName, sample);
+                    await Task.Delay(500);
+                }
+
+                imageDownloader.ImageStarted -= imageDownloader.LoadingStarted;
+                imageDownloader.ImageCompleted -= imageDownloader.LoadingCompleted;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to load the image: {0}", ex.Message);
+            }
+        }
+    }
+}
